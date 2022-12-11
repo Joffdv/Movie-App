2347d80b768b15b224ec9c96ce6c2b8cadb13a85
João Emanuel <107960713+Joffdv@users.noreply.github.com>
2022-12-11 12:04:38 +0000

    Finalizado

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,9 +1,11 @@
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import Meta from './Meta'
 
-export default function Layout({ children }) {
+export default function Layout({ children }: any) {
   return (
     <>
+      <Meta />
       <Navbar />
       <main>
         {children}
---
+++ components/Meta.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+export default function Meta({ keywords, description, title }: any) {
+  return (
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="keywords" content={keywords} />
+      <meta name="description" content={description} />
+      <meta charSet="utf-8" />
+      <link rel="icon" href="/favicon.ico" />
+      <title>{title}</title>
+    </Head>
+  )
+}
+
+Meta.defaultProps = {
+  title: 'WatchMe',
+  keywords: 'movie app, free movies, popular movies',
+  description: 'watch movie for free'
+}
